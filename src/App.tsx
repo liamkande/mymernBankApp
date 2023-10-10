75ae9e64df2ef4fa89f9ccf6ea0bdb63452c7915
liamkande <liamkande@icloud.com>
2023-10-10 13:19:07 -0700

    adds CardContainer for the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import CardContainer from './cardContainer'
+import './App.css'
 
 function App() {
+  const warningStyle = {
+    color: 'red',
+    fontSize: '.8rem',
+    fontWeight: 'bold',
+    marginBottom: '1rem',
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          This represents the culmination of my academic journey in the prestigious MIT Professional Certificate program in Coding: Full Stack Development – a capstone project that showcases my mastery of cutting-edge technology and programming expertise, solidifying my status as a forward-thinking and accomplished developer.
-      </header>
-    </div>
-  );
+    <CardContainer
+      align="center"
+      txtcolor="black"
+      header="Mern Bank App"
+      title="Welcome to my MIT Bank App!"
+      text="In our educational bank app, you can access checking and saving accounts."
+      body={
+        <>
+          <img
+            alt="bank"
+            src="https://img.icons8.com/external-photo3ideastudio-solid-photo3ideastudio/512/000000/external-bank-public-service-photo3ideastudio-solid-photo3ideastudio.png"
+            className="img-fluid"
+          />
+          <br />
+          <div
+            style={{
+              fontSize: '.6rem',
+            }}
+          >
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://icons8.com/icon/tx3AdinOQ2kf/bank"
+            >
+              Bank
+            </a>{' '}
+            icon by{' '}
+            <a target="_blank" rel="noreferrer" href="https://icons8.com">
+              Icons8
+            </a>
+          </div>
+          <p style={warningStyle}>For educational use only.</p>
+        </>
+      }
+    />
+  )
 }
 
-export default App;
+export default App
