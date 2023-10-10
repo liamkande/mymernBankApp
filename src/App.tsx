b26d8919db6e473d8315ac7c732e3a5adb0686b3
liamkande <liamkande@icloud.com>
2023-10-10 14:26:34 -0700

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Home from './pages/Home'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
